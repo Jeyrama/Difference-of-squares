47dc24f844a9e38e1cc0810b2a10dd43d2835fb6
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-08-18 14:59:13 -0700

    Update Difference-of-squares.js

--- Difference-of-squares.js
+++ Difference-of-squares.js
@@ -5,3 +5,12 @@ n natural numbers (1 <= n <= 100) and the square of their sum.
 
 
 // Solution
+
+function differenceOfSquares(x) {
+  let sum = 0, squareSum = 0, i;
+  for (i = 1; i <= x, i++) {
+    sum += i*i;
+    squareSum += i;
+  }
+  return squareSum*squareSum - sum;
+}
